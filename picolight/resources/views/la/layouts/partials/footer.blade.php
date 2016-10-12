9413425d88018a841296fba05cd6e8da7c55cbec
imrul kayes <kayes3878@gmail.com>
2016-10-12 13:03:39 +0600

    a corr

--- picolight/resources/views/la/layouts/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-@if(!isset($no_padding))
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        Powered by <a href="http://picolightdigital.com">Picolight solution Limited</a>
-    </div>
-    <strong>Copyright &copy; 2016
-</footer>
-@endif
